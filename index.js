c96c86e1f446da63a947e5add74b6577d3053267
Kaitlin Bunn <kabunn1991@gmail.com>
2018-08-06 11:32:48 -0400

    building hardcoded db

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-mongoose');
+// const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
 const app = express();
@@ -22,6 +22,12 @@ app.use(
   })
 );
 
+app.get ('/api/users', (req,res,next) => {
+  return res.json(
+    ['Nahkama', 'spooklings', 'felixiter', 'starlingstorm']
+  );
+})
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
@@ -34,7 +40,7 @@ function runServer(port = PORT) {
 }
 
 if (require.main === module) {
-  dbConnect();
+  // dbConnect();
   runServer();
 }
 
